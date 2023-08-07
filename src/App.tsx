bdc91ccd3f7d86566fd80e437816f17eb6f9dab3
Michael Knudson <michaelknudson@Michaels-MacBook-Pro.local>
2023-08-07 17:50:30 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Container from 'react-bootstrap/Container'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <Container fluid className='app'>
+      Góðan daginn, kæru vinir!
+    </Container>
+  )
+}
+
+export default App
